d65c02c05fa725a4ce6952b7325f6a6c754003cb
Matt Salem <msalem311@aol.com>
2020-09-16 13:57:01 +0000

    Uses printf to print message to stdout

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a message to stdout using printf
+ * Return: 0 upon completion
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
