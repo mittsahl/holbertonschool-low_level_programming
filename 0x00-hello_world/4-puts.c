8ff8775245f50f1f69b656cf34f034d379b8071c
Matt Salem <msalem311@aol.com>
2020-09-16 13:51:46 +0000

    Uses puts to print message to stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a message
+ * Return: 0 upon completion
+ */
+
+int main(void)
+{
+	char *str = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
