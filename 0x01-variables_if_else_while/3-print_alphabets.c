d01da2f09ef5008f1bcc0acd191d7f502be93668
Matt Salem <msalem311@aol.com>
2020-09-17 13:48:22 +0000

    Complete

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet twice
+ * Return: 0 upon completion
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter < 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter < 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
