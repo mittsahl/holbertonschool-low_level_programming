358dc5e4e7a252b0f58cc3a1013146a7d51cd6c9
Matt Salem <msalem311@aol.com>
2020-09-17 14:35:55 +0000

    Complete

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints out numbers 1-9 separated by commas
+ * Return: 0 upon completion
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
