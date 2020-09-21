c9b27ce166c54fdf3d1ef1723d68994b9dd2e234
Matt Salem <msalem311@aol.com>
2020-09-21 23:14:35 +0000

    Added description for function

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * _isalpha - checks if a char is a letter
+ * Return: 0 if not, 1 if it is a letter
+ * @c: ASCII value passed into function
+ */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
