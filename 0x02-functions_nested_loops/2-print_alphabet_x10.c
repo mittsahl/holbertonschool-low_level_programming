d1d8d1563c48995e7e960908a5d601ad338af53f
Matt Salem <msalem311@aol.com>
2020-09-21 15:17:05 +0000

    Complete

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int letter;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
