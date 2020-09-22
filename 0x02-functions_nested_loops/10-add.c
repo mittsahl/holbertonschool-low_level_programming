108fe728a5d770ed961bf73993176dcee109f604
Matt Salem <msalem311@aol.com>
2020-09-22 13:24:11 +0000

    First commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * Return: sum of both ints
+ * @a: int passed to function
+ * @b: int passed to function
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
