36a0d06151aea9e858e34f0740b05659a8b53148
mittsahl <msalem311@aol.com>
2020-09-24 18:08:35 +0000

    Changed format for checker

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,33 @@
 #include "holberton.h"
 
 /**
- * more_numbers - prints numbers 0 -14 x10
- * Return: void
+ * more_numbers - Function that prints 10 times the numbers, from 0 to 14.
+ *
  */
-
 void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int j;
+	int num1;
+	int num2;
 
-	for (j = 0; j < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 15; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i > 10)
-				_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
+			if (j >= 10)
+			{
+				num1 = j / 10;
+				num2 = j % 10;
+				_putchar('0' + num1);
+			}
+			else
+			{
+				num2 = j;
+			}
+			_putchar('0' + num2);
 		}
 		_putchar('\n');
 	}
 }
+
