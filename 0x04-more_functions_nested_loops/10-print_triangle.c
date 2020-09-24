e10beb19e8d2da33366b48d17057542050bad495
mittsahl <msalem311@aol.com>
2020-09-24 17:58:24 +0000

    Added description to function and removed whitespace

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+ * print_triangle - prints triangle of size size
+ * Return: void
+ * @size: the size of the triangle
+ */
+
 void print_triangle(int size)
 {
 	int i, j;
@@ -7,17 +13,17 @@ void print_triangle(int size)
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
-		{		
+		{
 			for (j = 0; j < size; j++)
 			{
-				if ( j < size - i - 1)
+				if (j < size - i - 1)
 					_putchar(' ');
 				else
 					_putchar('#');
-			}		
+			}
 			_putchar('\n');
 		}
 	}
 	else
 		_putchar('\n');
-}	
+}
