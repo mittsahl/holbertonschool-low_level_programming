f6b0f28efbbbcd73f777f139f95c0c888a4a72d8
mittsahl <msalem311@aol.com>
2020-09-24 07:05:33 +0000

    Multiplies two int and returns product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies 2 ints
+ * Return: product of both ints
+ * @a: int passed to function
+ * @b: int passed to function
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
