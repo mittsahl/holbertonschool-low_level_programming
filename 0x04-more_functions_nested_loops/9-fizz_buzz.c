cadf9417f750dd7ad35ec820c95e777c5e4959cc
mittsahl <msalem311@aol.com>
2020-09-24 19:14:28 +0000

    Added space at end if not 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,19 +1,34 @@
+#include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Fizz-Buzz Test
+ *
+ * Description: Prints the numbers from 1 to 100, but for multiples of three
+ * print Fizz instead of the number and for the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <  101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
-	}	
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+
 	return (0);
 }
+
