ea05dd3e09921443afcca8c1ca3da3e69d9628b2
mittsahl <msalem311@aol.com>
2020-09-24 15:00:54 +0000

    Prints line of n length

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints line n length
+ * Return: void
+ * @n: length of line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
