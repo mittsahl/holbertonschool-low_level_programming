d5c9e3259a4f2c2a5154ea0257f35198440851f0
mittsahl <msalem311@aol.com>
2020-09-28 15:07:45 +0000

    Prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints reverse of string
+ * Return: void
+ * @s: string frmo main passed to function
+ */
+
+void print_rev(char *s)
+{
+	char *ch = s;
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+		ch++;
+	for (; length >= 0; length--)
+	{
+		_putchar(*ch);
+		ch--;
+	}
+	_putchar('\n');
+}
