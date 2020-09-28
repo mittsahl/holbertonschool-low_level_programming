e57ea61b195b157ac0e5fb4c51f1c2982b4bec94
mittsahl <msalem311@aol.com>
2020-09-28 14:32:41 +0000

    Complete swaps two ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps two integers
+ * Return: void
+ * @a: int passed to function from main
+ * @b: int passed to function from main
+ */
+
+void swap_int(int *a, int *b)
+{
+  int tmp = *a;
+  *b =  *a;
+  *a = tmp;
+}
