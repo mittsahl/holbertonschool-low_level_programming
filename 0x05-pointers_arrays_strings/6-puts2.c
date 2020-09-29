545f476ab923aafdfe728b56ad35a52aa1d0df65
mittsahl <msalem311@aol.com>
2020-09-29 13:20:27 +0000

    Used pointer notation instead of array

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,19 @@
 #include "holberton.h"
 
 /**
- * puts2 - prints every other character of string
- * Return: void
- * @s: string passed to function from main
+ * puts2 - A function that prints every other character of a string
+ * @str: string agrument
+ *
+ * Return: void (No value returned)
  */
-
-void puts2(char *s)
+void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; s[i]!= '\0'; i += 2)
-		_putchar(s[i]);
+	for (i = 0; *str != '\0'; i++, str++)
+	{
+		if ((i % 2) == 0)
+			_putchar(*str);
+	}
 	_putchar('\n');
 }
