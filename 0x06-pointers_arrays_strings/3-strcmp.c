015cdafb0483d04c5c980aa2a1523a392dc7c17a
mittsahl <msalem311@aol.com>
2020-10-01 10:58:05 +0000

    Compares two strings and returns the difference

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two strings
+ *  Return: difference between the strings
+ *  @s1: string from main
+ *  @s2: string from main
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	for (; *s1 && *s2; s1++, s2++)
+	{
+		return (*s1 - *s2);
+	}
+	return (0);
+}
