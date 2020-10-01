fc975fe31623bf46312b06e44ed14909981df1c0
mittsahl <msalem311@aol.com>
2020-10-01 14:53:16 +0000

    Capitalizes every word in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * Return: pointer to string
+ * @s: string passed from main
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n') && _islower(s[i + 1]) == 1)
+			s[i + 1] -= 32;
+		if ((s[i] == ',' || s[i] == ';' || s[i] == '.') && _islower(s[i + 1]) == 1)
+			s[i + 1] -= 32;
+		if ((s[i] == '!' || s[i] == '?' || s[i] == '"') && _islower(s[i + 1]) == 1)
+			s[i + 1] -= 32;
+		if ((s[i] == '(' || s[i] == ')' || s[i] == '{') && _islower(s[i + 1]) == 1)
+			s[i + 1] -= 32;
+		if (s[i] == '}' && _islower(s[i + 1]) == 1)
+			s[i + 1] -= 32;
+	}
+	return (s);
+}
+
+/**
+ * _islower - checks if char is lowercase
+ * @c: char passed as arg
+ * Return: 1 if lowercase, else 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
