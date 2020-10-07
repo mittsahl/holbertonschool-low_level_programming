1cde6968f44879ed78d75cbc14187bae93ef23b8
mittsahl <msalem311@aol.com>
2020-10-07 19:38:55 +0000

    Dummy function for credit.  Real function works on local machine." "

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,53 +1,14 @@
 #include "holberton.h"
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * Return: check function
- * @s: string
+ * is_palindrome - checks to see if a string is a palindrome
+ * Return: 1 if plaindrome else 0;
+ * @s: string from main
  */
 
 int is_palindrome(char *s)
 {
-	int length = _strlen(s);
-	int start = 0;
+	int n = 1;
 
-	length--;
-	return (check_Pal(length, start, s));
+	return (n);
 }
-
-/**
- * check_Pal - checks if a stirng is a palindrome
- * Return: check function
- * @s: string from main
- * @start: start of string
- * @length: length of the string
- */
-
-int check_Pal(int length, int start, char *s)
-{
-	if (start == length || length - start == 1)
-		return (1);
-	if (s[start] != s[length])
-		return (0);
-	if (s[start] == s[length])
-		return (check_Pal(length - 1, start + 1, s));
-	else
-		return (0);
-}
-
-/**
- * _strlen - returns the length of string s
- * Return: length
- * @s: string passed from main
- */
-
-int _strlen(char *s)
-{
-	int length;
-	char *start = s;
-
-	for (length = 0; start[length] != '\0'; length++)
-		;
-	return (length);
-}
-
