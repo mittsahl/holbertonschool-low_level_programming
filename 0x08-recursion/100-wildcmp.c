3619495036051e9b74191de4d2beff5e0343462b
mittsahl <msalem311@aol.com>
2020-10-08 18:33:23 +0000

    Dummy function real function on localmachine

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -9,24 +9,9 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0')
-	{
-		if (*s2 == '*')
-		{
-			while (*++s2 == '*')
-				continue;
-			if (*s2 == '\0')
-				return (1);
-			while (wildcmp(s1, s2) == 0 && *s1 != '\0')
-				s1++;
-			return ((*s1 != '\0') ? 1 : 0);
-		}
-		if (*s2 != '?' && *s2 != *s1)
-			return (0);
-		s1++;
-		s2++;
-	}
-	while (*s2 == '*')
-		s2++;
-	return ((*s2 == '\0') ? 1 : 0);
+	char *tmp = s1;
+
+	s1 = s2;
+	s2 = tmp;
+	return (1);
 }
