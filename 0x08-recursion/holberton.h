b827694cc712838957a1944968d54a3c95bb0f2e
mittsahl <msalem311@aol.com>
2020-10-08 01:51:15 +0000

    Up to date with all protoypes for wildcmp

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -67,4 +67,5 @@ int is_palindrome(char *s);
 int check(int n, int i);
 int check_Pal(int length, int start, char *s);
 int wildcmp(char *s1, char *s2);
+int check_Wild(char *wildStr, char *check, int i, int j);
 #endif /*HOLBERTON_H */
