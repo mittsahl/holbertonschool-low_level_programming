e6e286aa1e128197f4e203edb4122ab2748ff954
mittsahl <msalem311@aol.com>
2020-10-09 17:33:16 +0000

    Prints argv[0] to command line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * Return: 0
+ * @argc: number of elements of argv
+ * @argv: array of strings
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	argc = argc;
+	return (0);
+}
