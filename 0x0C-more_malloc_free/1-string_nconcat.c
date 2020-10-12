b5338109c8e0d6926ffadd0ea5b220cda0552e8c
mittsahl <msalem311@aol.com>
2020-10-12 22:54:40 +0000

    Added NULL checks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *buf;
 
 	if (s1 == NULL && s2 == NULL)
-		return (buf);
+		return ("");
 	if (s1 != NULL)
 		s1length = _strlen(s1);
 	else 
