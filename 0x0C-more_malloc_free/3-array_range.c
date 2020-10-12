837a692b7c1d4d4639cefe1979f746df5947d67c
mittsahl <msalem311@aol.com>
2020-10-12 23:37:20 +0000

    Initializes an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* array_range - creates an array of integers
+* @min: min int value
+* @min: max int value
+* Return: pointer to the array else null
+*/
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *array;
+
+	if (min > max)
+		return (NULL);
+	if (max - min < 2)
+		i = 2;
+	else
+		i = max - min + 1;
+
+	array = malloc(i * 4);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i + min <= max; i++)
+		array[i] = i + min;
+	if (min == max)
+		array[i] = max;
+	return (array);
+}
+
