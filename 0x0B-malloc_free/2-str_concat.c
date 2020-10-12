5c5162d232b22e2087b581b8c7999f296ba882f2
mittsahl <msalem311@aol.com>
2020-10-12 14:12:09 +0000

    Changed NULL check to see if they are both NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,14 +15,16 @@ char *str_concat(char *s1, char *s2)
 	char *middle;
 	char *concat;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		return (s2);
-	if (s2 == 0)
+	if (s2 == NULL)
 		return (s1);
+	if (s1 == NUll && s2 == NULL)
+		return (NULL);
 	concat = malloc(firstLength + secondLength + 1);
 	middle = &concat[firstLength];
-	if (concat == 0)
-		return (0);
+	if (concat == NULL)
+		return (NULL);
 	_strcpy(concat, s1);
 	_strcpy(middle, s2);
 	return (concat);
