c963495431b6af2e9fe8a8cfaa14bd63574c4bb2
mittsahl <msalem311@aol.com>
2020-10-12 14:16:49 +0000

    Cahnged typo in NULL check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		return (s2);
 	if (s2 == NULL)
 		return (s1);
-	if (s1 == NUll && s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	concat = malloc(firstLength + secondLength + 1);
 	middle = &concat[firstLength];
