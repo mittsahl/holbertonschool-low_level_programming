80c0dbb0b024fef73db8d5a2bdcc3723bef64dc6
mittsahl <msalem311@aol.com>
2020-10-12 15:09:04 +0000

    Changed for loop to < height

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include "holberton.h"
 
 /**
-* free_grid - free a 2d array 
+* free_grid - free a 2d array
 * @grid: 2d array from main
 * @height: height of the array
 * Return: void
@@ -11,11 +11,11 @@
 void free_grid(int **grid, int height)
 {
 	int i;
-	
-	for (i = 0; i <= height; i++)
-	{	
+
+	for (i = 0; i < height; i++)
+	{
 		free(grid[i]);
-	}	
-	free(grid);;
+	}
+	free(grid);
 }
 
