16fa22688bc6c6607ac849ebb8771ca676009831
mittsahl <msalem311@aol.com>
2020-10-12 05:01:34 +0000

    Allocates memory for a 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+* alloc_grid - allocates a 2d array in memory
+* @width: width of the array
+* @height:  height of the array
+* Return: NULL if height or width <= 0 else the grid
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **twoDArray;
+	int i, j;
+
+	if (height <= 0 || width <= 0)
+		return (0);
+	twoDArray = (int **)malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		twoDArray[i] = (int *)malloc(sizeof(int) * width);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			twoDArray[i][j] = 0;
+	}
+	return (twoDArray);
+}
