ec3c9a7a392768fe10bdd88619fe523cd298bf1a
mittsahl <msalem311@aol.com>
2020-10-13 03:33:09 +0000

    Added tab

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		ptr = malloc(new_size);
 		if (ptr == NULL)
-		return (NULL);
+			return (NULL);
 	}
 	if (new_size < old_size)
 		old_size = new_size;
