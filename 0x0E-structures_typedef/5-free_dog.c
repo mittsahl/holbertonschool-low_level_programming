804b556578aeece96ce7f4a9b3f88fa72930bd0f
mittsahl <msalem311@aol.com>
2020-10-13 21:57:11 +0000

    Frees struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* free_dog - frees a dog
+* @d: dog from main
+* Return: void
+*/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
