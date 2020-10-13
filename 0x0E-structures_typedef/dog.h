71d9b0f972b356aaef501db325603f7dedd1ea4f
mittsahl <msalem311@aol.com>
2020-10-13 21:08:51 +0000

    Changed formatting

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - a dog
+ * @name: name of dog
+ * @age: how old dog is
+ * @owner: owner of dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
-	char *owner;	
+	char *owner;
 };
+
 #endif /* DOG_H */
