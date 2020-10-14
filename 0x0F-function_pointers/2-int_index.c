31b37361c358a5af2495f235ab51aab9ec2ecd8a
mittsahl <msalem311@aol.com>
2020-10-14 06:59:36 +0000

    Added NULL check for functions

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,14 +15,16 @@ int int_index(int *array, int size, int(*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	for (i = 0; i < size; i++)
+	if (cmp != NULL)
 	{
-		if (cmp(array[i]) != 0)
-			break;
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				break;
+		}
+		if (i < size)
+			return (i);
 	}
-	if (i < size)
-		return (i);
-	else
-		return (-1);
+	return (-1);
 }
 
