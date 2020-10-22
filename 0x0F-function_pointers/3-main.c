7abb22b7a05981ccb2c8f6810036b1b9a6adcd85
mittsahl <msalem311@aol.com>
2020-10-22 02:57:07 +0000

    Added checks for 1 char operator and if / or modul by 0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,24 +15,26 @@ int main(int argc, char **av)
 	int num1;
 	int num2;
 	int (*func)(int, int);
-
-	if (argc < 4)
+	char op;
+	
+	if (argc != 4)
 	{
 		printf("Error\n");
-		return (0);
+		exit(98);
 	}
-	func = get_op_func(av[2]);
-	if (func == NULL)
+	op = av[2][0];
+	func = get_op_func(op);
+	if (func == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
-		return (0);
+		exit(99);
 	}
 	num1 = atoi(av[1]);
 	num2 = atoi(av[3]);
-	if (num1 >= INT_MAX || num1 <= INT_MIN || num2 >= INT_MAX || num2 <= INT_MIN)
+	if ((op == '/' || op == '%') && n2 == 0)
 	{
-		printf("Integer overflow\n");
-		return (0);
+		printf("Error\n");
+		exit(42069);
 	}
 	printf("%d\n", func(num1, num2));
 	return (0);
