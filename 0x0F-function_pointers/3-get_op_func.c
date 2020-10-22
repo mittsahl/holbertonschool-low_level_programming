0899d810c05ecc928558edf702744db3f146116b
mittsahl <msalem311@aol.com>
2020-10-22 02:26:18 +0000

    Gets operation function based of operator char

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - function that returns a pointer to function
+ * that takes two ints and returns an in
+ * @s: operator to be matched
+ * Return: pointer to function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (*(ops + i)->op)
+	{
+		if (*(ops + i)->op == *s)
+			break;
+		i++;
+	}
+	return (*(ops + i)->f);
+}
