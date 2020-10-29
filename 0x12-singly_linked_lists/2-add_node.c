8f20eb28cc64ad2ff6f78c2e94a219700faff01a
mittsahl <msalem311@aol.com>
2020-10-29 17:25:41 +0000

    Adds new node start of linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* add_node - adds a node to linked list
+* @head:  head of linked list
+* @str: str to be duplicated
+* Return: address of new node NULL if failed;
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode = malloc(sizeof(list_t));
+
+	newNode->str = (char *)str;
+	newNode->len = _strlen((char *)str);
+	newNode->next = *head;
+	*head = newNode;
+	return (*head);
+}
+
+/**
+ * _strlen - returns the length of string s
+ * Return: length
+ * @s: string passed from main
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	char *start = s;
+
+	for (length = 0; start[length] != '\0'; length++)
+		;
+	return (length);
+}
