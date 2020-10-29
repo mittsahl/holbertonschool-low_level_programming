f2442ec716122c6e957f753763f155693f4bb3d7
mittsahl <msalem311@aol.com>
2020-10-29 16:18:00 +0000

    Prints a linked list and returns number of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* print_list - prints a linked list  
+* @h: head of linked list  
+* Return: number of nodes in linked list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t numOfNodes = 0;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		numOfNodes++;
+		h = h->next;
+	}
+	return (numOfNodes);
+}
+
