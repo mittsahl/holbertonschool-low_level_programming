113ef29fe4ea547f92cc91fb2627e0efcb9f8c95
mittsahl <msalem311@aol.com>
2020-11-03 17:42:45 +0000

    Added edge cases for first and last index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,22 +13,28 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp, *before, *newNode;
-	unsigned int index = 0;
+	unsigned int index = 0, len = (unsigned int)listint_len(*head);
 
+	if (idx > len)
+		return (NULL);
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
 	newNode->next = NULL;
-	if (*head == NULL && idx == 0)
+	if (idx == 0)
 	{
+		newNode->next = *head;
 		*head = newNode;
-		return (newNode);
+		return (*head);
 	}
-	else if (*head == NULL && idx > 0)
+	else if (idx == len + 1)
 	{
-		free(newNode);
-		return (NULL);
+		tmp = *head;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = newNode;
+		return (newNode);
 	}
 	else
 	{
@@ -45,3 +51,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (newNode);
 	}
 }
+
+/**
+* listint_len - returns the length of a linked list
+* @h: head of linked list
+* Return: length
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t length = 0;
+
+	while (h)
+	{
+		length++;
+		h = h->next;
+	}
+	return (length);
+}
+
