a43cacc05328f26deb9651192a1ae20cc09d89b5
mittsahl <msalem311@aol.com>
2020-11-09 19:19:47 +0000

    Fixed formatting

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
-* main - copies contents of one file to another file 
+* main - copies contents of one file to another file
 * @ac:  number of arguments
-* @av:  array of arguments 
+* @av:  array of arguments
 * Return: 0
 */
 
@@ -12,7 +12,7 @@ int main(int ac, char **av)
 	int from, to;
 	ssize_t readBytes, wroteBytes;
 	char *buf = malloc(1024);
-	
+
 	if (buf == NULL)
 		return (0);
 	if (ac != 3)
@@ -22,18 +22,18 @@ int main(int ac, char **av)
 	}
 	from = open(av[1], O_RDONLY);
 	to = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	readBytes = read(from, buf, 1024);	
-	wroteBytes = write(to, buf, readBytes); 
+	readBytes = read(from, buf, 1024);
+	wroteBytes = write(to, buf, readBytes);
 	if (from < 0 || readBytes < 0)
 	{
 		printf("Error: Can't read from file %s\n", av[1]);
 		exit(98);
-	}	
+	}
 	if (to < 0 || wroteBytes < 0)
 	{
 		printf("Error: Can't write to %s\n", av[2]);
 		exit(99);
-	}	
+	}
 	to = close(to);
 	from = close(from);
 	if (to < 0 || from < 0)
